b6ec1c453184228d2eaaa63c8a77833f6b24e1e9
Kocsis Laszlo <kocsis.laszlo@kadeno.ro>
2024-04-29 18:22:14 +0300

    Customize default error screen

--- android/src/main/java/com/masteratul/exceptionhandler/DefaultErrorScreen.java
+++ android/src/main/java/com/masteratul/exceptionhandler/DefaultErrorScreen.java
@@ -1,98 +1,105 @@
-package com.masteratul.exceptionhandler;
+<?xml version="1.0" encoding="utf-8"?>
+<ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:fillViewport="true"
+    android:background="#D9EAE1"
+    android:orientation="vertical"
+    android:visibility="visible">
+    <LinearLayout
+        android:padding="20dp"
+        android:layout_width="match_parent"
+        android:orientation="vertical"
+        android:layout_height="wrap_content">
+        <TextView
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="24sp"
+            android:gravity="center"
+            android:textStyle="bold"
+            android:textColor="#1F292C"
+            android:padding="16dp"
+            android:text="A system error has occurred"/>
+        <ImageView
+            android:tint="#EA3455"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:adjustViewBounds="true"
+            android:maxWidth="80dp"
+            android:maxHeight="80dp"
+            android:scaleType="fitCenter"
+            android:src="@drawable/alert"/>
 
-import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+        <TextView
+            android:id="@+id/textView"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:gravity="center"
+            android:textColor="#1F292C"
+            android:paddingHorizontal="10dp"
+            android:paddingTop="16dp"
+            android:textSize="16sp"
+            android:text="The Loopeli app may no longer function as expected." />
+        <TextView
+            android:id="@+id/textView2"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:gravity="center"
+            android:textColor="#1F292C"
+            android:paddingHorizontal="10dp"
+            android:paddingTop="5dp"
+            android:textSize="16sp"
+            android:text="Please restart the app to continue." />
+        <LinearLayout
+            android:layout_width="match_parent"
+            android:gravity="bottom"
+            android:paddingTop="10dp"
+            android:layout_height="wrap_content">
+            <Button
+                android:id="@+id/eh_restart_button"
+                android:layout_weight="1"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"
+                android:backgroundTint="#385858"
+                android:textColor="#ffffff"
+                android:paddingVertical="24dp"
+                android:textSize="16sp"
+                android:text="RESTART"/>
+            <Button
+                android:id="@+id/eh_quit_button"
+                android:layout_weight="1"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"
+                android:backgroundTint="#385858"
+                android:textColor="#ffffff"
+                android:paddingVertical="24dp"
+                android:textSize="16sp"
+                android:text="QUIT"/>
+        </LinearLayout>
+        <Button
+            android:id="@+id/eh_show_details_button"
+            android:layout_width="match_parent"
+            android:layout_height="40dp"
+            android:text="SHOW DETAILS"
+            android:textSize="18sp"
+            android:background="@android:color/transparent"
+            android:textStyle="bold"
+            android:textColor="#1F292C"
+            android:layout_marginTop="4dp"
+            android:layout_marginBottom="12dp"
+            android:gravity="center"/>
+        <TextView
+            android:id="@+id/eh_stack_trace_text_view"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:background="#eeeeee"
+            android:padding="16dp"
+            android:textColor="#000000"
+            android:text=""
+            android:visibility="gone"/>
 
-public class DefaultErrorScreen extends Activity {
-    private static String TAG = "RN_ERROR_HANDLER";
-    private Button quitButton;
-    private Button relaunchButton;
-    private Button showDetailsButton;
-    private TextView stackTraceView;
+    </LinearLayout>
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String stackTraceString = "StackTrace unavailable";
-        try {
-            stackTraceString = getIntent().getExtras().getString("stack_trace_string");
-        }
-        catch (Exception e) {
-            Log.e(TAG, String.format("Was not able to get StackTrace: %s", e.getMessage()));
-        }
-        setContentView(R.layout.default_error_screen);
-        quitButton = (Button) findViewById(R.id.eh_quit_button);
-        relaunchButton = (Button) findViewById(R.id.eh_restart_button);
-        showDetailsButton = (Button) findViewById(R.id.eh_show_details_button);
-        stackTraceView = (TextView) findViewById(R.id.eh_stack_trace_text_view);
-        stackTraceView.setText(stackTraceString);
 
-        showDetailsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int stackTraceViewVisibility = stackTraceView.getVisibility();
-                if(stackTraceViewVisibility == View.VISIBLE){
-                    stackTraceView.setVisibility(View.GONE);
-                    showDetailsButton.setText("SHOW DETAILS");
-                }else{
-                    stackTraceView.setVisibility(View.VISIBLE);
-                    showDetailsButton.setText("HIDE DETAILS");
-                }
-            }
-        });
-
-        relaunchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                doRestart(getApplicationContext());
-            }
-        });
-
-        quitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.exit(0);
-            }
-        });
-    }
-
-    public static void doRestart(Context c) {
-        try {
-            if (c != null) {
-                PackageManager pm = c.getPackageManager();
-                if (pm != null) {
-                    Intent mStartActivity = pm.getLaunchIntentForPackage(
-                            c.getPackageName()
-                    );
-                    if (mStartActivity != null) {
-                        mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        int mPendingIntentId = 654311;
-                        PendingIntent mPendingIntent = PendingIntent
-                                .getActivity(c, mPendingIntentId, mStartActivity,
-                                        PendingIntent.FLAG_CANCEL_CURRENT);
-                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
-                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
-                        System.exit(0);
-                    } else {
-                        throw new Exception("Was not able to restart application, mStartActivity null");
-                    }
-                } else {
-                    throw new Exception("Was not able to restart application, PM null");
-                }
-            } else {
-                throw new Exception("Was not able to restart application, Context null");
-            }
-        } catch (Exception ex) {
-            Log.e(TAG, "Was not able to restart application");
-        }
-    }
-}
+</ScrollView>
